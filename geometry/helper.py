c6bfb108c2a7678fea5ca69c78070e351f123fda
Chad\YOGA <glinskyc@gmail.com>
2019-05-16 17:20:44 -0600

    Adds geometry / Helical Gears notebook, including many svg graphics in img folder.

--- geometry/helper.py
+++ geometry/helper.py
@@ -1,4 +1,7 @@
-"""Helper module for Jupyter notebooks."""
+# Copyright 2019 Drivetrain Hub LLC
+# For non-commercial use only.  For commercial products and services, visit https://www.drivetrainhub.com.
+
+"""Helper module for geometry notebooks."""
 
 from IPython.display import HTML
 import random
@@ -50,3 +53,31 @@ def round_degrees(rad) -> int:
 
     if rad is not None:
         return int(round(degrees(rad), 0))
+
+
+def html_table(headings, rows):
+    """Display an HTML table in a Jupyter notebook.
+
+    :param headings: List of heading strings.  Can include MathJax notation.
+    :param rows: List of lists to represent rows of data.
+    :return: None
+    """
+
+    html_str = '<table style="margin-left: 0">'
+
+    html_str += '<tr>'
+
+    for h in headings:
+        html_str += f'<th>{h}</th>'
+
+    html_str += '</tr>'
+
+    for row in rows:
+        html_str += '<tr>'
+        for c in row:
+            html_str += f'<td>{c}</td>'
+
+        html_str += '</tr>'
+
+    html_str += '</table>'
+    display(HTML(html_str))
