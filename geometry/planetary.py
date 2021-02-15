90505b9e8384986fee2e24b5404df91d684fc5e2
Chad\YOGA <glinskyc@gmail.com>
2021-02-15 16:13:54 -0700

    Progress on planetary gears.

--- geometry/planetary.py
+++ geometry/planetary.py
@@ -4,14 +4,24 @@
 """Notebook module for planetary gear geometry."""
 
 import numpy as np
-from math import pi
+from math import pi, cos, sin
+from typing import Tuple
 
 
+Point = Tuple[float, float]
+Coord = Tuple[np.ndarray, np.ndarray]
 PHI_CIRCLE = np.linspace(0, 2*pi)
 
 
-def circle_curve(radius, phi=PHI_CIRCLE, x_center=0, y_center=0):
+def circle_curve(radius: float, phi:np.ndarray = PHI_CIRCLE, x_center: float = 0, y_center: float = 0) -> Coord:
     x = radius * np.cos(phi) + x_center
     y = radius * np.sin(phi) + y_center
+
+    return x, y
+
+
+def polar_to_cartesian(radius: float, phi: float) -> Point:
+    x = radius * cos(phi)
+    y = radius * sin(phi)
 
     return x, y
